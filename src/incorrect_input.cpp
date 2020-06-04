2423257e38e13353e26c878e2b35807b8812fc64
Denchik <89137714168@mail.ru>
2020-06-04 09:19:50 +0200

    test Travis

--- src/incorrect_input.cpp
+++ src/incorrect_input.cpp
@@ -8,7 +8,9 @@ int incorrect_input(char num[255], int* test_flag)
 {
     int i, numCheck, intnum;
     std::stringstream ss;
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 4; i++)
+
+    {
         numCheck = num[i];
 
         if (numCheck < 48 || numCheck > 57) {
